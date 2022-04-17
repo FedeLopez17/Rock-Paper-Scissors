99714cf46f8cc025087adbe399d8a1aaa4003fe7
FedeLopez17 <fedelopez1712@gmail.com>
2022-04-17 17:07:40 -0300

    Change function name and add some commits

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,37 +1,37 @@
 function computerPlay(){
     choice = Math.floor(Math.random() * 100);
     result = (choice >= 1 && choice < 34) ? "Rock" : (choice > 66) ? "Scissors" : "Paper";
-    console.log(result);
+    console.log(result); // Just for test purposes
     return result;
 }
 
-function playerSelection(){
+function playerSelect(){
     choice = prompt("Input your choice").toLowerCase();
     let upperCaseFirstLetter = choice.charAt(0).toUpperCase();
     choice = upperCaseFirstLetter.concat(choice.substr(1).toLowerCase());
-    console.log(choice);
+    console.log(choice); // Just for test purposes
     return choice;
 }
 
 function playRound(){
     const computerSelection = computerPlay();
-    const playerSelection1 = playerSelection();
-    if (playerSelection1 === computerSelection) {
+    const playerSelection = playerSelect();
+    if (playerSelection === computerSelection) {
         return "It's a tie!";
     }
-    else if (playerSelection1 === "Rock"){
+    else if (playerSelection === "Rock"){
         if (computerSelection === "Scissors"){
             return "You win! Rock beats Scissors";
         }
         return "You lose! Paper beats Rock";
     }
-    else if (playerSelection1 === "Paper"){
+    else if (playerSelection === "Paper"){
         if (computerSelection === "Rock"){
             return "You win! Paper beats Rock";
         }
         return "You lose! Scissors beat Paper";
     }
-    else if (playerSelection1 === "Scissors"){
+    else if (playerSelection === "Scissors"){
         if (computerSelection === "Paper"){
             return "You win! Scissors beat Paper";
         }
